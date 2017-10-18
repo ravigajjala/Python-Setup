0c599c0ace0431e196871fc83cce874a1bca76cc
Alwin Paul <alwin.paul7489@gmail.com>
2017-10-18 23:50:43 +0530

    style update formanage user popup

--- src/app/manage-green-house/manage-green-house.component.ts
+++ src/app/manage-green-house/manage-green-house.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AppSharedService } from '../providers/services/app-shared.service';
+import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+import { User } from '../providers/classes/plantInfo.class';
 
 @Component({
   selector: 'app-manage-green-house',
@@ -8,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 export class ManageGreenHouseComponent implements OnInit {
 
 
-	constructor() { }
 
-	ngOnInit() {
-	}
+  public locationId;
+  public locations = [];
+  public showEditBox = false;
+  public editUserName;
+  public editUserEmail;
+  public editLocationId;
+
+  constructor(public appSharedService: AppSharedService,
+    public dialogRef: MdDialogRef<ManageUsersComponent>,
+    @Inject(MD_DIALOG_DATA) public currentUsers: any) { }
+
+
+  addUser(condition) {
+    this.showEditBox = condition;
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  saveUser() {
+    const newUser = new User();
+    newUser.id = this.currentUsers.length;
+    newUser.name = this.editUserName;
+    newUser.email = this.editUserEmail;
+    newUser.state = this.editLocationId.state;
+    newUser.city = this.editLocationId.city;
+
+    this.currentUsers.push(newUser);
+    this.showEditBox = false;
+    this.clearEditFields();
+  }
+
+  clearEditFields() {
+    this.editUserName = null;
+    this.editUserEmail = null;
+    this.editLocationId = null;
+  }
+
+  deleteUser(i) {
+    this.currentUsers.splice(i, 1);
+  }
 
 }
