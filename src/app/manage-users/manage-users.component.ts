3b26eabe8a1a41b5746a56a427e05fbbe3974b4e
Alwin Paul <alwin.paul7489@gmail.com>
2017-10-18 21:44:51 +0530

    manage users done

--- src/app/manage-users/manage-users.component.ts
+++ src/app/manage-users/manage-users.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AppSharedService } from '../providers/services/app-shared.service';
+import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+import { User } from '../providers/classes/plantInfo.class';
 
 @Component({
   selector: 'app-manage-users',
@@ -9,14 +11,46 @@ import { AppSharedService } from '../providers/services/app-shared.service';
 export class ManageUsersComponent {
 
 
-	public locationId;
-	public locations = [];
-	public showEditBox = false;
+  public locationId;
+  public locations = [];
+  public showEditBox = false;
+  public editUserName;
+  public editUserEmail;
+  public editLocationId;
 
-  constructor(public appSharedService: AppSharedService) { }
+  constructor(public appSharedService: AppSharedService,
+    public dialogRef: MdDialogRef<ManageUsersComponent>,
+    @Inject(MD_DIALOG_DATA) public currentUsers: any) { }
 
 
   addUser(condition) {
-  	this.showEditBox = condition;
+    this.showEditBox = condition;
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  saveUser() {
+    const newUser = new User();
+    newUser.id = this.currentUsers.length;
+    newUser.name = this.editUserName;
+    newUser.email = this.editUserEmail;
+    newUser.state = this.editLocationId.state;
+    newUser.city = this.editLocationId.city;
+
+    this.currentUsers.push(newUser);
+    this.showEditBox = false;
+    this.clearEditFields();
+  }
+
+  clearEditFields() {
+    this.editUserName = null;
+    this.editUserEmail = null;
+    this.editLocationId = null;
+  }
+
+  deleteUser(i) {
+    this.currentUsers.splice(i, 1);
   }
 }
